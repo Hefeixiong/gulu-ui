02c4b914ed341a5cdb60aeaec95d2553059fcfcd
hefeixiong <1074535830@qq.com>
2021-12-01 21:36:06 +0800

    Home init

---
+++ src/router.ts
@@ -0,0 +1,15 @@
+import {createRouter, createWebHashHistory} from 'vue-router';
+import Home from './views/Home.vue';
+
+
+const history = createWebHashHistory();
+export const router = createRouter({
+  history: history,
+  routes: [
+    {path: '/', component: Home}
+  ]
+});
+
+router.afterEach(() => {
+  console.log('路由切换了');
+});
---
+++ src/views/Home.vue
@@ -0,0 +1,4 @@
+<template>
+  <div>xxxx</div>
+</template>
+
