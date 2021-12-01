387b363bf0156bdb818535cf24d6154671cc88d3
hefeixiong <1074535830@qq.com>
2021-12-02 00:18:33 +0800

    可以接受value和update:value事件

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>switch</div>
+  <Switch :value="buttonStatus" @update:value="buttonStatus = $event"/>
 </template>
 
 <script lang="ts">
+import Switch from '../lib/Switch.vue';
+import {ref} from 'vue';
 
+export default {
+  components: {Switch},
+  setup() {
+    const buttonStatus = ref(false);
+    return {buttonStatus};
+  }
+};
 </script>
 
 <style lang="scss" scoped>
