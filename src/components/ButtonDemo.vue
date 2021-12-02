04febfe3e37acdb5e061c1af9b54c1d1e27140f4
hefeixiong <1074535830@qq.com>
2021-12-02 15:03:22 +0800

    初始化button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,9 +1,19 @@
 <template>
-  <div>button</div>
+  <Button loading>加载中</Button>
+  <Button>加载完毕</Button>
 </template>
 
 <script lang="ts">
-
+import Button from '../lib/Button.vue'
+export default  {
+  components: {Button},
+  setup() {
+    const onClick = () => {
+      console.log('hi')
+    }
+    return {onClick}
+  }
+}
 </script>
 
 <style lang="scss" scoped>
---
+++ src/lib/Button.vue
@@ -0,0 +1,51 @@
+<template>
+  <button class="gulu-button" :class="classes" :disabled="disabled">
+    <span v-if="loading" class="gulu-loadingIndicator"></span>
+    sss
+    <slot/>
+  </button>
+</template>
+
+<script lang="ts">
+import {computed} from 'vue';
+
+export default {
+  props: {
+    theme: {
+      type: String,
+      default: 'button'
+    },
+    size: {
+      type: String,
+      default: 'normal'
+    },
+    level: {
+      type: String,
+      default: 'normal'
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup(props) {
+    const {theme , size , level} = props
+    const classes = computed(() => {
+      return {
+        [`gulu-theme-${theme}`]: theme,
+        [`gulu-size-${size}`]: size,
+        [`gulu-level-${level}`]: level,
+      }
+    })
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
