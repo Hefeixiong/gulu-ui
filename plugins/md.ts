1aa531c6ff22d5106c47063411fc8cab6316b897
hefeixiong <1074535830@qq.com>
2021-12-10 01:42:42 +0800

    支持markdown文件

---
+++ plugins/md.ts
@@ -0,0 +1,32 @@
+// @ts-nocheck
+import path from 'path';
+import {marked} from 'marked'
+import fs from 'fs';
+
+const mdToJs = str =>  {
+  const content = JSON.stringify(marked(str))
+  return `export default ${content}`
+}
+
+export function md() {
+  return {
+    configureServer: [ //用于开发
+      async ({app}) => {
+      app.use(async (ctx , next) => {
+        if (ctx.path.endsWith('.md')) {
+          ctx.type = 'js'
+          const filePath = path.join(process.cwd(), ctx.path)
+          ctx.body = mdToJs(fs.readFileSync(filePath).toString())
+        } else {
+          await next()
+        }
+      })
+      }
+    ],
+
+    transforms: [{ //用于rollup //插件
+      test: context => context.path.endsWith('.md'),
+      transform: ({code}) => mdToJs(code)
+    }]
+  }
+}
